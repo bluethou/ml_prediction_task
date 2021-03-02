e232151326b1a9774650dfd91a1d5db291f51277
bluethou <61516838+bluethou@users.noreply.github.com>
2021-03-02 23:30:19 +0900

    Update pyspark_script.py

--- files/pyspark_script.py
+++ files/pyspark_script.py
@@ -1,38 +1,36 @@
 
 from pyspark.ml.feature import OneHotEncoder
 from pyspark.sql.types import IntegerType
-# from pyspark.ml.feature import StringIndexer
-
-df = spark.read.options(header='True').csv("train_vali.csv")
-
-df = df.drop('_c0')
-for col in df.columns:
-	df = df.withColumn(col, df[col].cast(IntegerType()))
-
-colnames = []
-outputcols = []
-for i in range(1,10):
-	colnames.append("categorical_feature_"+str(i))
-	outputcols.append("categoryVec"+str(i))
-
-encoder = OneHotEncoder(inputCols=colnames, outputCols=outputcols)
-model = encoder.fit(df)
-encoded = model.transform(df)
-encoded = encoded.drop(*colnames)
-
-from pyspark.ml.feature import VectorAssembler
-assembler = VectorAssembler( inputCols=encoded.columns, outputCol='features')
-transform_encoded = assembler.transform(encoded)
-
-
-
-split = transform_encoded.randomSplit([0.8,0.2], 1234)
-
-train = split[0]
-vali = split[1]
-
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+# from pyspark.ml.feature import StringIndexer
+
+df1 = spark.read.options(header='True').csv("train.csv")
+df2 = spark.read.options(header='True').csv("validation.csv")
+
+def data_transform(df):
+	df = df.drop('_c0')
+	for col in df.columns:
+		df = df.withColumn(col, df[col].cast(IntegerType()))
+
+	colnames = []
+	outputcols = []
+	for i in range(1,10):
+		colnames.append("categorical_feature_"+str(i))
+		outputcols.append("categoryVec"+str(i))
+
+	encoder = OneHotEncoder(inputCols=colnames, outputCols=outputcols)
+	model = encoder.fit(df)
+	encoded = model.transform(df)
+	encoded = encoded.drop(*colnames)
+
+	from pyspark.ml.feature import VectorAssembler
+	assembler = VectorAssembler( inputCols=encoded.columns, outputCol='features')
+	transform_encoded = assembler.transform(encoded)
+	return transform_encoded
+
+train = data_transform(df1)
+vali = data_transform(df2)
 
 lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8, labelCol = 'label')
 lrModel = lr.fit(train)
